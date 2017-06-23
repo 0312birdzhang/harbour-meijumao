9f68f59c025b6b4459583f70040a53af829fcd5d
BirdZhang <0312birdzhang@gmail.com>
2017-06-23 14:07:36 +0800

    work py

--- qml/py/main.py
+++ qml/py/main.py
@@ -1,13 +1,18 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+'''
+Created on 2017年6月23日
+
+@author: debo.zhang
+'''
 import sys,os
 import urllib.request
 import urllib.parse
-import pyotherside
 import json
-from basedir import *
+import re
 from bs4 import BeautifulSoup
 import logging
 
-target=HOME+"/Downloads/"
 __AUTHOR__ = "BirdZhang"
 
 _meijumao = "http://www.meijumao.net"
@@ -21,18 +26,35 @@ __index__ = [
 ]
 
 
+
+def get(url):
+    headers = {
+        'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/52.0.2743.116 Safari/537.36 Edge/15.15063',
+        'Host': 'www.meijumao.net'
+        }
+    req = urllib.request.Request(url,headers=headers)
+    try:
+        response = urllib.request.urlopen(req,timeout=30)
+        allhtml = response.read()
+        return allhtml
+    except Exception as e:
+        print(str(e))
+        #logging.debug(traceback.format_exc())
+        return ''
+    
+
 def index():
     return __index__
 
 # get categories
 def list_categories(article):
     html = get(_meijumao + article)
-    soup = BeautifulSoup(html, "html5lib")
+    soup = BeautifulSoup(html, "html.parser")
     listing = []
     for urls in soup.find_all("a", attrs={"data-remote": "true"}):
         listing.append({
            "action" : "list_sections",
-           "section": urls.get("href").replace(_meijumao, "")
+           "section": urls.get("href").replace(_meijumao, ""),
            "label" : urls.div.get_text()
 
     })
@@ -45,11 +67,7 @@ def list_sections(section):
     if section == "#":
         return
     html = get(_meijumao + section)
-    soup = BeautifulSoup(html, "html5lib")
-
-    will_page = soup.find("ul", attrs={"id": "will_page"})
-    if will_page:
-        pass
+    soup = BeautifulSoup(html, "html.parser")
     listing = []
     sections_map = {}
     for section in soup.find_all("article"):
@@ -70,148 +88,88 @@ def list_sections(section):
         if will_page[-1].find("a").get("href") != "#":
             sections_map["pre_page"] = True
             sections_map["pre_section"] = will_page[-1].find("a").get("href")
-        
-    return json.loads(sections_map)
+    return json.dumps(sections_map)
 
 
-#def list_series(series, seriesname, fanart_image):
-#    html = get(_meijumao + series)
-#    soup_series = BeautifulSoup(html, "html5lib")
+def list_series(series):
+    html = get(_meijumao + series)
+    soup_series = BeautifulSoup(html, "html5lib")
 
-#    listing = []
-#    for serie in soup_series.find_all(
-#            "div", attrs={
-#            "class": "col-lg-1 col-md-2 col-sm-4 col-xs-4"}):
-#        if not serie.a:
-#            continue
-#        list_item = xbmcgui.ListItem(
-#            label=serie.a.get_text().replace(
-#                " ", "").replace(
-#                "\n", ""))
-#        if not serie.a.get("href").startswith("/"):
-#            continue
-#        url = '{0}?action=play_video&episode={1}&name={2}'.format(
-#            _url,
-#            serie.a.get("href"),
-#            "[COLOR green]" +
-#            seriesname +
-#            "[/COLOR] [COLOR yellow]" +
-#            serie.a.get_text().replace(
-#                " ",
-#                "").replace(
-#                "\n",
-#                "").encode("utf-8") +
-#            "[/COLOR]")
-#        is_folder = False
-#        listing.append((url, list_item, is_folder))
+    listing = []
+    for serie in soup_series.find_all(
+            "div", attrs={
+            "class": "col-lg-1 col-md-2 col-sm-4 col-xs-4"}):
+        if not serie.a:
+            continue
 
-#    xbmcplugin.addDirectoryItems(_handle, listing, len(listing))
-#    xbmcplugin.endOfDirectory(_handle)
+        if not serie.a.get("href").startswith("/"):
+            continue
+        listing.append({
+                "action":"play_video",
+                "label":serie.a.get_text().replace(" ", "").replace("\n", ""),
+                "episode":serie.a.get("href")
+            })
+    return listing
+  
 
 
-#def list_playsource(episode, name):
-#    html = get(_meijumao + episode)
-#    soup_source = BeautifulSoup(html, "html5lib")
-#    listing = []
-#    for source in soup_source.find_all(
-#            "a", attrs={
-#            "class": "button button-small button-rounded"}):
-#        list_item = xbmcgui.ListItem(label=source.get_text())
-#        if source.get("href").startswith("http"):
-#            continue
-#        # url = '{0}?action=play_video&episode={1}&name={2}'.format(_url, source.get("href"),name)
-#        listing.append((source.get("href"), name))
-#    if len(listing) == 0:
-#        dialog.ok(__addonname__, '没有找到视频源')
-#        return
-#    else:
-#        play_video(listing[0])
+def list_playsource(episode):
+    html = get(_meijumao + episode)
+    soup_source = BeautifulSoup(html, "html5lib")
+    listing = []
+    for source in soup_source.find_all(
+            "a", attrs={
+            "class": "button button-small button-rounded"}):
+#         if source.get("href").startswith("http"):
+#             continue
+        listing.append({"href":source.get("href"),
+                        "source":source.get_text()})
+    return listing
 
 
-#def play_video(episode, name):
-#    """
-#    Play a video by the provided path.
-#    :param path: str
-#    :return: None
-#    """
-#    episode = episode.replace("show_episode?", "play_episode?")
-#    html = get(_meijumao + episode)
-#    if not html:
-#        dialog.ok(__addonname__, '没有找到视频源，播放出错')
-#        return
-#    soup_js = BeautifulSoup(html, "html5lib")
-#    title = ""
-#    if soup_js.find_all("h1"):
-#        title = soup_js.find_all("h1")[0].get_text()
-#    if soup_js.find_all("li", attrs={"class": "active"}):
-#        title += " - " + soup_js.find_all("li",
-#                                          attrs={"class": "active"})[0].get_text()
-#    play_url = ""
-#    for script in soup_js.find_all('script'):
-#        matched = re.search('http.*m3u8.*\"', script.get_text())
-#        if matched:
-#            play_url = matched.group().replace(
-#                "\"",
-#                "").replace(
-#                "&amp;",
-#                "&").replace(
-#                "->application/x-mpegURL",
-#                "")
-#    if len(play_url) == 0:
-#        dialog.ok(__addonname__, '没有找到视频源，播放出错')
-#        return
-#    play_item = xbmcgui.ListItem(name)
-#    play_item.setInfo(type="Video", infoLabels={"Title": name})
-#    # Pass the item to the Kodi player.
-#    xbmcplugin.setResolvedUrl(_handle, True, listitem=play_item)
-#    # directly play the item.
-#    xbmc.Player().play(play_url, play_item)
+def play_video(episode):
+    """
+    Play a video by the provided path.
+    :param path: str
+    :return: None
+    """
+    episode = episode.replace("show_episode?", "play_episode?")
+    html = get(_meijumao + episode)
+    if not html:
+        return None
+    soup_js = BeautifulSoup(html, "html5lib")
+    title = ""
+    if soup_js.find_all("h1"):
+        title = soup_js.find_all("h1")[0].get_text()
+    if soup_js.find_all("li", attrs={"class": "active"}):
+        title += " - " + soup_js.find_all("li",
+                                          attrs={"class": "active"})[0].get_text()
+    play_url = ""
+    for script in soup_js.find_all('script'):
+        matched = re.search('http.*m3u8.*\"', script.get_text())
+        if matched:
+            return matched.group().replace(
+                "\"",
+                "").replace(
+                "&amp;",
+                "&").replace(
+                "->application/x-mpegURL",
+                "")
+    if len(play_url) == 0:
+        return None
+
+def search(keyword):
+    '''
+    Search 
+    :param keyword:
+    '''
+    p_url = "/search?q="
+    url = p_url + keyword.decode('utf-8').encode('gb2312')
+    return list_sections(url)
 
 
-#def search():
-#    keyboard = xbmc.Keyboard('', '请输入搜索内容')
-#    xbmc.sleep(1500)
-#    keyboard.doModal()
-#    if (keyboard.isConfirmed()):
-#        keyword = keyboard.getText()
-#        p_url = "/search?q="
-#        url = p_url + \
-#            urllib.quote_plus(keyword.decode('utf-8').encode('gb2312'))
-#        list_sections(url)
-#    else:
-#        return
 
-#def router(paramstring):
-#    """
-#    Router function that calls other functions
-#    depending on the provided paramstring
-#    :param paramstring:
-#    :return:
-#    """
-#    # Parse a URL-encoded paramstring to the dictionary of
-#    # {<parameter>: <value>} elements
-#    params = dict(parse_qsl(paramstring))
-#    # Check the parameters passed to the plugin
-#    if params:
-#        if params['action'] == 'index_router':
-#            if params['article'] == '/search':
-#                search()
-#            elif params['article'] == '/maogetvs' or params['article'] == '/alltvs' or params['article'] == '/populartvs':
-#                list_sections(params['article'])
-#            elif params['article'] == '/categories':
-#                list_categories('/alltvs')
-#        elif params['action'] == 'list_sections':
-#            list_sections(params['section'])
-#        elif params['action'] == 'list_series':
-#            list_series(
-#                params['series'],
-#                params["seriesname"],
-#                params["fanart_image"])
-#        elif params['action'] == 'list_playsource':
-#            list_playsource(params['episode'], params["name"])
-#        elif params['action'] == 'play_video':
-#            play_video(params['episode'], params["name"])
-
-#    else:
-#        index()
-
+if __name__ == "__main__":
+#     print(list_sections("/maogetvs")) #list_series
+#     print(list_series("/tvs/110"))
+    print(play_video("/tvs/110/show_episode?episode=214"))
